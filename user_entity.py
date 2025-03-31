ad92f9f11b902aee359a15b665332adb8dbc8735
swordshik <beforlifeandmoon@gmail.com>
2025-03-31 08:01:20 -0400

    created user entity.py

---
+++ user_entity.py
@@ -0,0 +1,17 @@
+class User:
+
+    def __init__(self, username: str, password: str):
+        self.username = username
+        self.__password = password
+
+    def __str__(self):
+        return f"Username = {self.username})"
+    
+    def __repr__(self):
+        return f"User(username={self.username}, password=****)"
+    
+    def get_password(self):
+        return self.__password
+    
+    def set_password(self, password: str):
+        self.__password = password
