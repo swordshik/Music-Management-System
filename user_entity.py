f18b287139f95e793fb88c7cd4ef373934e46dd0
swordshik <beforlifeandmoon@gmail.com>
2025-04-11 09:41:14 -0400

    deleted admin etuty, because we changed plan. DAO was renamed to Users_dao and subjected to users table in database. Database was deleted, since there were changes in configurations of it.

--- user_entity.py
+++ user_entity.py
@@ -1,8 +1,15 @@
 class User:
 
-    def __init__(self, username: str, password: str):
+    def __init__(self, username, email,  password):
         self.username = username
+        self.email = email
         self.__password = password
+
+    def get_username(self):
+        return self.username
+    
+    def get_email(self):
+        return self.email
     
     def set_password(self, password: str):
         self.__password = password
---
+++ users_dao.py
@@ -0,0 +1,27 @@
+import sqlite3
+from user_entity import User
+
+class UsersDAO:
+
+    def __init__(self, db_path='song_managing.db'):
+        self.db_path = db_path
+        
+    def insert_user(self, username, email, password):
+        conn = sqlite3.connect(self.db_path)
+        cursor = conn.cursor()
+        user1 = User(username, email, password)
+        cursor.execute('''
+            INSERT INTO users (username, email, password) VALUES (?, ?, ?)
+        ''', (user1.get_username(), user1.get_email(), user1.get_password()))
+        conn.commit()
+        conn.close()
+
+    def get_user(self, username):
+        conn = sqlite3.connect(self.db_path)
+        cursor = conn.cursor()
+        cursor.execute('''
+            SELECT * FROM users WHERE username=?
+        ''', (username,))
+        user = cursor.fetchone()
+        conn.close()
+        return user
